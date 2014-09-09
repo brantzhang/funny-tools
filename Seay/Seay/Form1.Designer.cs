9aefe21433b8ddaac86bd6efa5e28b35fede73fc
brantzhang <hj-zhang@foxmail.com>
2014-09-09 18:24:27 +0800

    一个别人写的刷票工具，目前还有一些问题，问题应该出在cookies和验证码那一块。

---
+++ Seay/Seay/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Seay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(235, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始刷票";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "作品ID：";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(85, 24);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(135, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 82);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "停止刷票";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "刷票次数：0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "注册邮箱前缀：";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(135, 21);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "regtest";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(37, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(329, 60);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "tips：把要注册的用户名放到这个程序目录下面，一行一个，\r\n程序自动注册刷票成功的用户会把邮箱放在本程序下面的\r\nregsuccess.txt文件里面，密码统一" +
+    "是woshimima\r\n注册邮箱前缀是要注册的邮箱前面的字符，程序会在前缀\r\n后面加一个数字，比如regtest1@qq.com";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(445, 199);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Seay";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Seay/Seay/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Seay
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+
+        }
+
+        string cookies = "";
+        string html = "";
+
+        public string post(bool isreg, string posturl, string postdata, string cookie)
+        {
+            try
+            {
+
+
+                byte[] data = Encoding.Default.GetBytes(postdata);
+
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(posturl);
+                req.Method = "POST";
+
+                req.ContentLength = data.Length;
+                req.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+                req.Accept = "application/json, text/javascript, */*; q=0.01";
+                req.Referer = "http://www.douguo.com/signup.html";
+                req.UserAgent = "a";
+                if (isreg)
+                {
+                    req.Headers.Add("cookie", "bdshare_firstime=1409932112477; CNZZDATA30029854=cnzz_eid%3D1361503193-1409928798-http%253A%252F%252Fwww.douguo.com%252F%26ntime%3D1409928798; Hm_lvt_a2b4c73b05ad5f4b724f1393c72b67cc=1409932115; Hm_lpvt_a2b4c73b05ad5f4b724f1393c72b67cc=1409932664; last_show=1409932115499; showNums=3; username=u09924428671886; dg_auths=a%3A5%3A%7Bs%3A10%3A%22session_id%22%3Bs%3A32%3A%22cc03899888dcc1b708aab2292c9b5981%22%3Bs%3A10%3A%22ip_address%22%3Bs%3A14%3A%22101.40.134.123%22%3Bs%3A10%3A%22user_agent%22%3Bs%3A1%3A%22a%22%3Bs%3A13%3A%22last_activity%22%3Bi%3A1409932663%3Bs%3A9%3A%22mycaptcha%22%3Bs%3A4%3A%225AZU%22%3B%7D0b9a0b3ca73451e20a5648d82ba46244");
+                }
+                else
+                {
+                    req.Headers.Add("cookie", cookies);
+                }
+
+                Stream s = req.GetRequestStream();
+                s.Write(data, 0, data.Length);
+                s.Close();
+
+                HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+                StreamReader sr = new StreamReader(res.GetResponseStream(), Encoding.UTF8);
+                html = sr.ReadToEnd();
+                sr.Close();
+                res.Close();
+
+                cookies = res.Headers["set-cookie"].Replace("path=/,", "");
+
+                
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return html;
+        }
+
+
+        public void start()
+        {
+            if (textBox1.Text.Trim() == "")
+            {
+                MessageBox.Show("请输入要刷票的ID");
+                return;
+            }
+
+            if (!File.Exists("1.txt"))
+            {
+                MessageBox.Show("请把用户名列表一行一个放到这个程序目录下面，并且文件名为1.txt");
+                return;
+            }
+
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+
+            string[] names = File.ReadAllLines("1.txt",Encoding.GetEncoding("utf-8"));
+
+            for (int i = 1; i < names.Length; i++)
+            {
+                //注册及登陆
+                try
+                {
+                    if (post(true, "http://www.douguo.com/ajax/signup", "&user=" + textBox2.Text.Trim() + i + "@qq.com&pwd=woshimima&nickname=" + names[i]  + "&code=5AZU&lvyun=1", "").Contains("\"status\":\"ERR\""))
+                    {
+                        continue;
+                    }
+
+                    if (post(false, "http://www.douguo.com/uajax/addDishFav", "&d_id=" + textBox1.Text.Trim(), cookies).Contains("\"status\":\"OK\""))
+                    {
+                        File.AppendAllText("regsuccess.txt","注册邮箱为："+textBox2.Text.Trim() + i + "@qq.com\r\n");
+                    }
+
+                    label2.Text = "刷票次数：" + i;
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+
+            }
+
+        }
+        Thread th = null;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            th = new Thread(start);
+            th.Start();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (th != null && th.ThreadState != ThreadState.Aborted)
+            {
+                th.Abort();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (th != null && th.ThreadState != ThreadState.Aborted)
+            {
+                th.Abort();
+            }
+        }
+    }
+}
